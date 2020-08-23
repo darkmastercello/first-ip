73c7fe168c784b4bdd9294836e926aba96fac45f
Marcellus Githua <marcellusgithua@gmail.com>
2020-08-23 16:39:42 +0300

    add border, and change font for app goal

---
+++ akan.js
@@ -0,0 +1,2 @@
+var days = [Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday]
+var months = [January, February, March, April, May, June, July, August, September, October, November, December]
