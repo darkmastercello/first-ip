ed8015c3cbd3a7dc6677b1fc54976889a7685adb
Marcellus Githua <marcellusgithua@gmail.com>
2020-08-23 17:05:47 +0300

    add female and male akan names arrays

--- akan.js
+++ akan.js
@@ -1,2 +1,4 @@
 var days = [Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday]
 var months = [January, February, March, April, May, June, July, August, September, October, November, December]
+var maleAkanNames = [Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame]
+var femaleAkanNames = [Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama]
