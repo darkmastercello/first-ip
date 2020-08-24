45854db83188562fb3eed7591e12b350f5bef532
Marcellus Githua <marcellusgithua@gmail.com>
2020-08-24 22:09:31 +0300

    add variables

--- akan.js
+++ akan.js
@@ -1,4 +1,31 @@
-var days = [Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday]
-var months = [January, February, March, April, May, June, July, August, September, October, November, December]
-var maleAkanNames = [Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame]
-var femaleAkanNames = [Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama]
+window.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM fully loaded and parsed")
+}
+)
+let CC = document.querySelector("#century").value
+let MM = document.querySelector("#month")
+let YY = document.querySelector("#year")
+let DD = document.querySelector("#datemonth")
+
+
+
+
+
+
+
+
+/*function fillForm(birthday) {
+  console.log("form is submitted")
+}
+function dayOfMonth() {
+  return (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
+};
+
+
+
+let myForm = document.querySelector("#detailForm")
+myForm.addEventListener("submit",)
+
+
+/*var result = dayOfMonth();
+alert(result)*/
