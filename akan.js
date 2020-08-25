32dd764a06872a9b13fa39257b4e33a655f72417
Marcellus Githua <marcellusgithua@gmail.com>
2020-08-25 13:20:30 +0300

    change querySelector to elementbyid

--- akan.js
+++ akan.js
@@ -1,21 +1,19 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-  console.log("DOM fully loaded and parsed")
-}
-)
 let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Afua", "Afua", "Ama"];
 
+
 let day = function () {
-  let CC = document.querySelector("#century").value
-  let MM = document.querySelector("#month").value
-  let YY = document.querySelector("#year").value
-  let DD = document.querySelector("#date").value
+  let CC = parseInt(document.getElementById("century").value);
+  let YY = parseInt(document.getElementById("year").value);
+  let MM = parseInt(document.getElementById("month").value);
+  let DD = parseInt(document.getElementById("date").value);
 }
+
 function calculate() {
   day()
   dayOfTheWeek = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
   return (Math.floor(dayOfTheWeek))
-  alert(dayOfTheWeek)
+  console.log(dayOfTheWeek)
 }
 
 
