f8198ffa768a03b9c0bb409f8300cf2bed73375d
Marcellus Githua <marcellusgithua@gmail.com>
2020-08-25 16:14:42 +0300

    edit calculation function

--- akan.js
+++ akan.js
@@ -13,24 +13,28 @@ function calculate() {
   day()
   dayOfTheWeek = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
   return (Math.floor(dayOfTheWeek))
-  console.log(dayOfTheWeek)
+  alert(dayOfTheWeek)
+}
+let result = function () {
+  dayOfTheWeek = calculate();
+  checkgender();
+}
+var checkgender = function () {
+  calculate();
+  var userGender = document.getElementsByName("morf"); //Try get document by name.same name for each gender
+  if (userGender[0].checked == true) {
+    gender = "male";
+  } else if (userGender[1].checked == true) {
+    gender = "female";
+  }
+  else {
+  }
+
+
+
+  alert("form is submitted")
 }
 
 
 
 
-/*function fillForm(birthday) {
-  console.log("form is submitted")
-}
-function dayOfMonth() {
-  return (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
-};
-
-
-
-let myForm = document.querySelector("#detailForm")
-myForm.addEventListener("submit",)
-
-
-/*var result = dayOfMonth();
-alert(result)*/
