6fc37b9e8ed48c5da8d61b48be4d37b2adff39f1
Marcellus Githua <marcellusgithua@gmail.com>
2020-08-27 10:48:19 +0300

    add arrays

--- akan.js
+++ akan.js
@@ -6,23 +6,36 @@ let genders = document.getElementsByName("radios");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault()
-  function dayValidator() {
-    if (month === 2 && Number(year) % 4 === 0) {
-      if (day > 28 || day < 1) {
-        return false;
-        alert("enter valid date!")
-      } else if (month === 2 && day > 29) {
-        return false;
-      } else if (month === 2 && day < 1) {
-        return false;
-      } else {
-        return true;
-      }
-    } else if (day < 1 || day > 31) {
-      return false;
-    } else {
-      return true;
-    }
-  }
+
 })
 
+
+let maleAkanNames = [
+  "Kwasi",
+  "Kwadwo",
+  "Kwabena",
+  "Kwaku",
+  "Yaw",
+  "Kofi",
+  "Kwame"]
+
+
+let femaleAkanNames = [
+  "Akosua",
+  "Adwoa",
+  "Abenaa",
+  "Akua",
+  "Yaa",
+  "Afua",
+  "Ama"
+];
+
+let daysOfTheWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
