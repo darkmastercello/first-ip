8a1d1cec4f13d7425f089ac0932f5797f600b4fb
Marcellus Githua <marcellusgithua@gmail.com>
2020-08-27 09:44:47 +0300

    add limits

--- akan.js
+++ akan.js
@@ -1,21 +1,28 @@
-let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Afua", "Afua", "Ama"];
+let year = document.getElementById("year").value
+let month = document.getElementById("month").value
+let day = document.getElementById("monthday").value
+let genders = document.getElementsByName("radios");
 
 
-function getName() {
-  let CC = parseInt(document.getElementById("century").value);
-  let YY = parseInt(document.getElementById("year").value);
-  let MM = parseInt(document.getElementById("month").value);
-  let DD = parseInt(document.getElementById("date").value);
-}
-if (mm < 0 || mm > 32) {
-  alert("Please enter a valid month!")
-}
-else if (MM === 2 && DD === 29 && YY % 4 === 0) {
-  alert("You must have been born on a leap year!!")
-}
-
-
-
-
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  function dayValidator() {
+    if (month === 2 && Number(year) % 4 === 0) {
+      if (day > 28 || day < 1) {
+        return false;
+        alert("enter valid date!")
+      } else if (month === 2 && day > 29) {
+        return false;
+      } else if (month === 2 && day < 1) {
+        return false;
+      } else {
+        return true;
+      }
+    } else if (day < 1 || day > 31) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+})
 
