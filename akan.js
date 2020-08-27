13179b72aedfa14522db317f28706a555b4a9553
Marcellus Githua <marcellusgithua@gmail.com>
2020-08-27 09:43:30 +0300

    minor edits to html

--- akan.js
+++ akan.js
@@ -2,38 +2,19 @@ let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Afua", "Afua", "Ama"];
 
 
-let day = function () {
+function getName() {
   let CC = parseInt(document.getElementById("century").value);
   let YY = parseInt(document.getElementById("year").value);
   let MM = parseInt(document.getElementById("month").value);
   let DD = parseInt(document.getElementById("date").value);
 }
-
-function calculate() {
-  day()
-  dayOfTheWeek = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
-  return (Math.floor(dayOfTheWeek))
-  alert(dayOfTheWeek)
+if (mm < 0 || mm > 32) {
+  alert("Please enter a valid month!")
 }
-let result = function () {
-  dayOfTheWeek = calculate();
-  checkgender();
+else if (MM === 2 && DD === 29 && YY % 4 === 0) {
+  alert("You must have been born on a leap year!!")
 }
-var checkgender = function () {
-  calculate();
-  var userGender = document.getElementsByName("morf"); //Try get document by name.same name for each gender
-  if (userGender[0].checked == true) {
-    gender = "male";
-  } else if (userGender[1].checked == true) {
-    gender = "female";
-  }
-  else {
-  }
 
-
-
-  alert("form is submitted")
-}
 
 
 
